4dbd5a226b8245f86aec0de6a90be8bac9905462
Suriya <suriyaadhi007@gmail.com>
2026-05-21 14:48:48 +0530

    project base structure

--- src/models/train_baselines.py
+++ src/models/train_baselines.py
@@ -2,12 +2,11 @@ import mlflow
 import mlflow.sklearn
 
 mlflow.set_tracking_uri("file:./mlruns")
+
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
-print("Script started")
 
 
 X, y = load_iris(return_X_y=True)
@@ -15,15 +14,11 @@ X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
-    test_size=0.2,
+    test_size=0.7,
     random_state=42
 )
 
-print("Data loaded")
-
 with mlflow.start_run():
-
-    print("MLflow run started")
 
     model = RandomForestClassifier(
         n_estimators=100,
@@ -39,13 +34,9 @@ with mlflow.start_run():
         predictions
     )
 
-    print("Accuracy calculated")
-
     mlflow.log_param("n_estimators", 100)
     mlflow.log_param("max_depth", 5)
 
     mlflow.log_metric("accuracy", accuracy)
 
     print(f"Accuracy: {accuracy}")
-
-print("Script completed")
--- test_logger.py
+++ test_logger.py
@@ -3,3 +3,5 @@ from src.utils.logger import setup_logger
 logger = setup_logger()
 
 logger.info("Logger works successfully")
+
+
